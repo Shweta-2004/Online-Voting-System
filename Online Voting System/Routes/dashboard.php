6937697bd358cb386400f703e36232928fc531b7
Shweta-2004 <shindeshweta9495@gmail.com>
2023-10-22 19:04:26 +0530

    First Commit

---
+++ Online Voting System/Routes/dashboard.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+       <title>ONLINE VOTING SYSTEM-dashboard</title>
+</head>
+<body>
+    <button>Back</button>
+    <button>Logout</button>
+    <h1>ONLINE VOTING SYSTEM<h1>
+    <hr>
+    <div id="Profile"></div>
+    <div id="Groups"></div>
+</body>
+</html>
